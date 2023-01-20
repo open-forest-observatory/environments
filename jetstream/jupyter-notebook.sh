a73fb8107de5fe01f9a71d093f07e30256aa132a
Derek Young <derek.j.n.young@gmail.com>
2023-01-20 13:35:40 -0800

    Add comment with documentation

--- jetstream/jupyter-notebook.sh
+++ jetstream/jupyter-notebook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This is based on Jetstream2 instructions: https://docs.jetstream-cloud.org/general/jupyter/
+# and also based on the script provided by Jetstream2 at /software/scripts/jupyter-ip.sh
+
 if [ -z "$JS2_PUBLIC_IP" ]; then
 JS2_PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 fi
