98364172d6f47d5622cbd7c61c266dac7ebdc9ad
Derek Young <derek.j.n.young@gmail.com>
2023-06-09 12:58:16 -0700

    Add jupyter lab IP script

---
+++ jetstream/jupyter-lab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is based on Jetstream2 instructions: https://docs.jetstream-cloud.org/general/jupyter/
+# and also based on the script provided by Jetstream2 at /software/scripts/jupyter-ip.sh
+
+if [ -z "$JS2_PUBLIC_IP" ]; then
+JS2_PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+fi
+
+#echo "JS2 IP is: $JS2_PUBLIC_IP"
+
+jupyter lab --no-browser --ip=0.0.0.0 2>&1 | sed s/127.0.0.1/${JS2_PUBLIC_IP}/g
